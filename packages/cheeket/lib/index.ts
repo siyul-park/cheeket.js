d7db81fbed566333a6b7f2a38c527ea9afd72928
siyual-park <siyual.bak@gmail.com>
2020-10-25 21:59:46 +0900

    feature/fix-export

--- packages/cheeket/lib/index.ts
+++ packages/cheeket/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as LookUp } from "./look-up/look-up";
+export { default as asSingleton } from "./provider/as-singleton";
 export { default as Container } from "./container/container";
 export { default } from "./container/container";
