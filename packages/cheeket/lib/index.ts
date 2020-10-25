8688aa1ce2ff81a010d19c6f3760f17ee2501e53
Ara Park <kdPark0723@gmail.com>
2020-10-25 10:03:20 +0900

    feature/fix_export (#5)

--- packages/cheeket/lib/index.ts
+++ packages/cheeket/lib/index.ts
@@ -1,5 +1,11 @@
-export { default as Container } from "./container/container";
-export * from "./binding/binding-in-and-for-syntax";
+export {
+  default as BindingInAndForSyntax,
+  BindingInSyntax,
+  BindingForSyntax,
+} from "./binding/binding-in-and-for-syntax";
+export { default as BindingToSyntax } from "./binding/binding-to-syntax";
 export { default as AccessLimiter } from "./contrant/access-limiter";
 export { default as Lifecycle } from "./contrant/lifecycle";
 export { default as LookUp } from "./look-up/look-up";
+export { default as Container } from "./container/container";
+export { default } from "./container/container";
