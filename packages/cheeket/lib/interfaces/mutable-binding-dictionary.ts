bb49ea726edc79559fe4f2a427e3d62b30c0ecc7
Ara Park <kdPark0723@gmail.com>
2020-11-22 18:01:17 +0900

    feat: add createChildContainer (#39)

---
+++ packages/cheeket/lib/interfaces/mutable-binding-dictionary.ts
@@ -0,0 +1,13 @@
+import Token from "./token";
+import Provider from "./provider";
+import BindingDictionary from "./binding-dictionary";
+
+interface MutableBindingDictionary extends BindingDictionary {
+  set<T>(token: Token<T>, provider: Provider<T>): void;
+  get<T>(token: Token<T>): Provider<T> | undefined;
+  getAll<T>(token: Token<T>): Provider<T>[];
+  delete<T>(token: Token<T>): void;
+  has<T>(token: Token<T>): boolean;
+}
+
+export default MutableBindingDictionary;
