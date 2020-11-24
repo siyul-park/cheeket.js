5c2badb75f05527a72ac118807d78046e2d10be3
Ara Park <kdPark0723@gmail.com>
2020-11-24 19:51:50 +0900

    fix: context swiching (#43)

--- packages/cheeket/lib/context/context.ts
+++ packages/cheeket/lib/context/context.ts
@@ -7,36 +7,43 @@ import { EventType } from "../event";
 class Context implements interfaces.Context {
   id = Symbol(uniqid());
 
-  readonly #bindingDictionary: interfaces.BindingDictionary;
+  readonly #bindingDictionaries: interfaces.BindingDictionary[];
 
   readonly #eventProducer: interfaces.EventProducer;
 
   public readonly children = new Set<interfaces.Context>();
 
   constructor(
-    bindingDictionary: interfaces.BindingDictionary,
+    bindingDictionaries: interfaces.BindingDictionary[],
     eventProducer: interfaces.EventProducer,
     public request: interfaces.Request<unknown>,
     public parent?: interfaces.Context
   ) {
-    this.#bindingDictionary = bindingDictionary;
+    this.#bindingDictionaries = bindingDictionaries;
     this.#eventProducer = eventProducer;
   }
 
   async resolve<T>(token: interfaces.Token<T>): Promise<T> {
-    const provider = this.#bindingDictionary.get(token);
-    if (provider !== undefined) {
-      return this.resolveProvider(provider, token);
-    }
-
-    throw new CantResolveError(token, this);
+    const [
+      provider,
+      bindingDictionaryIndex,
+    ] = this.findProviderAndBindingDictionaryIndex(token);
+    return this.resolveProvider(provider, token, bindingDictionaryIndex);
   }
 
   async resolveAll<T>(token: interfaces.Token<T>): Promise<T[]> {
-    const providers = this.#bindingDictionary.getAll(token);
-    if (providers.length > 0) {
+    const providersAndIndexList = this.findProvidersAndBindingDictionaryIndex(
+      token
+    );
+    if (providersAndIndexList.length > 0) {
       return Promise.all(
-        providers.map((provider) => this.resolveProvider(provider, token))
+        providersAndIndexList
+          .map(([providers, bindingDictionaryIndex]) =>
+            providers.map((provider) =>
+              this.resolveProvider(provider, token, bindingDictionaryIndex)
+            )
+          )
+          .reduce((acc, cur) => acc.concat(cur), [])
       );
     }
 
@@ -45,9 +52,10 @@ class Context implements interfaces.Context {
 
   private async resolveProvider<T>(
     provider: interfaces.Provider<T>,
-    token: interfaces.Token<T>
+    token: interfaces.Token<T>,
+    bindingDictionaryIndex: number
   ): Promise<T> {
-    const context = this.createChild(token);
+    const context = this.createChild(token, bindingDictionaryIndex);
     const value = await provider(context);
     context.request.resolved = value;
 
@@ -56,10 +64,13 @@ class Context implements interfaces.Context {
     return value;
   }
 
-  createChild<T>(token: interfaces.Token<T>): Context {
+  private createChild<T>(
+    token: interfaces.Token<T>,
+    bindingDictionaryIndex: number
+  ): Context {
     const request = new Request(token);
     const context = new Context(
-      this.#bindingDictionary,
+      this.#bindingDictionaries.slice(bindingDictionaryIndex),
       this.#eventProducer,
       request,
       this
@@ -68,6 +79,31 @@ class Context implements interfaces.Context {
     this.children.add(context);
 
     return context;
+  }
+
+  private findProviderAndBindingDictionaryIndex<T>(
+    token: interfaces.Token<T>
+  ): [interfaces.Provider<T>, number] {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < this.#bindingDictionaries.length; i++) {
+      const provider = this.#bindingDictionaries[i].get(token);
+      if (provider !== undefined) return [provider, i];
+    }
+
+    throw new CantResolveError(token, this);
+  }
+
+  private findProvidersAndBindingDictionaryIndex<T>(
+    token: interfaces.Token<T>
+  ): [interfaces.Provider<T>[], number][] {
+    const result: [interfaces.Provider<T>[], number][] = [];
+
+    this.#bindingDictionaries.forEach((bindingDictionary, i) => {
+      const providers = this.#bindingDictionaries[i].getAll(token);
+      if (providers.length !== 0) result.push([providers, i]);
+    });
+
+    return result;
   }
 
   emit(event: interfaces.EventToken, ...values: any[]): boolean {
