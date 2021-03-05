37b23b50add996485dbe72ed6a63dda27cafb884
Ara Park <kdPark0723@gmail.com>
2021-03-05 23:37:11 +0900

    feat: optimise build (#101)
    * refactoring
    
    * add uglify
    
    * lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
+const gulpif = require('gulp-if');
 const ts = require('gulp-typescript');
+const uglify = require('gulp-uglify');
 
 const tsProject = ts.createProject(
   process.env.NODE_ENV ? `tsconfig.${process.env.NODE_ENV.toLowerCase()}.json` : 'tsconfig.json'
 );
 
-gulp.task('default', () => {
+gulp.task('build', () => {
   return tsProject.src()
     .pipe(tsProject())
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('uglify', () => {
+  return gulp.src('dist/**/*.js')
+    .pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', gulp.series('build', 'uglify'));
