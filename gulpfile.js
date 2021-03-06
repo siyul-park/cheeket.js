4bfa680a58d163f7ac770af1619494a8c495fef3
Ara Park <kdPark0723@gmail.com>
2021-03-06 10:32:08 +0900

    feat: migration gulp (#102)
    * 마이그래이션
    
    * refactoring
    
    * refactoring
    
    * change to parse tsconfig
    
    * refactoring
    
    * fix exclude error
    
    * refactoring
    
    * refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,74 @@
-const gulp = require('gulp');
+const fs = require('fs');
+const path = require("path");
+
+const { src, dest, series, lastRun } = require('gulp');
 const gulpif = require('gulp-if');
 const ts = require('gulp-typescript');
 const uglify = require('gulp-uglify');
+const merge = require('deepmerge')
 
-const tsProject = ts.createProject(
-  process.env.NODE_ENV ? `tsconfig.${process.env.NODE_ENV.toLowerCase()}.json` : 'tsconfig.json'
-);
+function getTsconfigName() {
+  if (!process.env.NODE_ENV) return 'tsconfig.json';
 
-gulp.task('build', () => {
-  return tsProject.src()
+  const specificConfig = `tsconfig.${process.env.NODE_ENV.toLowerCase()}.json`;
+  if (fs.existsSync(specificConfig)) {
+    return specificConfig;
+  }
+
+  return 'tsconfig.json';
+}
+
+function getFinalTsConfig(config, currentPath) {
+  const { extends: parentConfigPath, ...rest } = config;
+
+  if (parentConfigPath) {
+    const finalParentConfigPath = path.join(currentPath, parentConfigPath);
+    const parentProject = ts.createProject(finalParentConfigPath);
+
+    const parentConfig = getFinalTsConfig(parentProject.rawConfig, parentProject.projectDirectory);
+    return merge(parentConfig, rest);
+  }
+  return rest;
+}
+
+function getTsconfig(tsProject) {
+  return getFinalTsConfig(tsProject.rawConfig, tsProject.projectDirectory)
+}
+
+function getTsFilenames(tsProject) {
+  const tsconfig = getTsconfig(tsProject);
+
+  const { fileNames, errors } = tsProject.typescript.parseJsonConfigFileContent(
+    tsconfig,
+    tsProject.typescript.sys,
+    path.resolve(tsProject.projectDirectory),
+    undefined,
+    tsProject.configFileName
+  );
+
+  for (const error of errors) {
+    console.error(error.messageText);
+  }
+
+  return fileNames;
+}
+
+const tsProject = ts.createProject(getTsconfigName());
+const tsconfig = getTsconfig(tsProject);
+const tsFilenames = getTsFilenames(tsProject);
+
+function compile() {
+  return src(tsFilenames, { sourcemaps: true, since: lastRun(compile) })
     .pipe(tsProject())
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(dest(tsconfig.compilerOptions.outDir));
+}
 
-gulp.task('uglify', () => {
-  return gulp.src('dist/**/*.js')
+function compression() {
+  return src(path.join(tsconfig.compilerOptions.outDir, '**/*.js'), { sourcemaps: true, since: lastRun(compression) })
     .pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(dest(tsconfig.compilerOptions.outDir));
+}
 
-gulp.task('default', gulp.series('build', 'uglify'));
+const build = series(compile, compression);
+
+exports.default = series(build);
