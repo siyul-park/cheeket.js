e8c93ddc1630374534458c0530584166f383dfed
Ara Park <kdPark0723@gmail.com>
2021-03-06 16:48:16 +0900

    feat: pass koa context in init (#52)
    * Add koa context in initializer
    
    * fix
    
    * fix
    
    * fix error

--- packages/@cheeket-koa/lib/initializer.ts
+++ packages/@cheeket-koa/lib/initializer.ts
@@ -1,8 +1,17 @@
 import { interfaces } from "cheeket";
+import { DefaultContext, DefaultState, ParameterizedContext } from "koa";
 
-interface Initializer {
+interface Initializer<
+  StateT = DefaultState,
+  ContextT = DefaultContext,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ResponseBodyT = any
+> {
   initRootContainer(container: interfaces.Container): void;
-  initContextContainer(container: interfaces.Container): void;
+  initContextContainer(
+    container: interfaces.Container,
+    context: ParameterizedContext<StateT, ContextT, ResponseBodyT>
+  ): void;
 }
 
 export default Initializer;
