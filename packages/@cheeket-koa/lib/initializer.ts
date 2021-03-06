d330605f8af4f9b0b63bb41208ddb01b85119f3a
siyual.park <siyual.bak@gmail.com>
2021-03-06 16:34:35 +0900

    Add koa context in initializer

--- packages/@cheeket-koa/lib/initializer.ts
+++ packages/@cheeket-koa/lib/initializer.ts
@@ -1,8 +1,16 @@
 import { interfaces } from "cheeket";
+import { DefaultContext, DefaultState, ParameterizedContext } from "koa";
 
-interface Initializer {
+interface Initializer<
+  StateT = DefaultState,
+  ContextT = DefaultContext,
+  ResponseBodyT = never
+> {
   initRootContainer(container: interfaces.Container): void;
-  initContextContainer(container: interfaces.Container): void;
+  initContextContainer(
+    container: interfaces.Container,
+    context: ParameterizedContext<StateT, ContextT, ResponseBodyT>
+  ): void;
 }
 
 export default Initializer;
