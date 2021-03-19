acb2e80c839a7a7ef704840ddd172bcf885cc15e
Ara Park <siyual.bak@gmail.com>
2021-03-19 14:45:11 +0900

    feat: refactoring package (#120)
    * change package name
    
    * add DeepPartial
    
    * add more type
    
    * Add writable
    
    * change lock and json

---
+++ commons/util-types/lib/join.ts
@@ -0,0 +1,7 @@
+type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K}${"" extends P ? "" : "."}${P}`
+    : never
+  : never;
+
+export default Join;
---
+++ commons/util-types/lib/nil.ts
@@ -0,0 +1,3 @@
+type Nil = null | undefined;
+
+export default Nil;
