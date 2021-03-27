98991e5439987993784658ae2abac5cc0b0cd423
Ara Park <siyual.bak@gmail.com>
2021-03-27 18:24:40 +0900

    feat: add dependency initializer (#75)
    * Add DependencyInitializer
    
    * Add use

--- packages/cheeket-koa/lib/index.ts
+++ packages/cheeket-koa/lib/index.ts
@@ -2,6 +2,8 @@ import dependency from "./dependency";
 import * as Token from "./token";
 
 export { default as ContainerContext } from "./container-context";
+export { default as DependencyInitializer } from "./dependency-initializer";
+export { default as use } from "./use";
 export { dependency };
 export { Token };
 export default dependency;
