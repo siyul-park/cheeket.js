29872486659af08e207ee6746d8e6b1ecbdd4e99
siyual-park <kdpark0723@gmail.com>
2021-03-27 18:22:37 +0900

    Add use

--- packages/cheeket-koa/lib/index.ts
+++ packages/cheeket-koa/lib/index.ts
@@ -3,6 +3,7 @@ import * as Token from "./token";
 
 export { default as ContainerContext } from "./container-context";
 export { default as DependencyInitializer } from "./dependency-initializer";
+export { default as use } from "./use";
 export { dependency };
 export { Token };
 export default dependency;
