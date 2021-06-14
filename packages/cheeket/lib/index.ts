0b6d5e5b69e3699b589ae7b61e14db1aa61c2e10
siyual-park <kdpark0723@gmail.com>
2021-06-14 20:04:35 +0900

    refactoring

--- packages/cheeket/lib/index.ts
+++ packages/cheeket/lib/index.ts
@@ -2,7 +2,7 @@ export * from "./token";
 export { Binder } from "./binder";
 export { ResolveChain, ResolveError } from "./chain";
 export { Container, RootContainer } from "./container";
-export { Context, DefaultState } from "./context";
+export { Context, DefaultState, ContainerEventEmitter } from "./context";
 export { Middleware, MiddlewarePipeline } from "./middleware";
 export {
   Provider,
--- packages/cheeket/lib/provider/in-container-scope.ts
+++ packages/cheeket/lib/provider/in-container-scope.ts
@@ -1,9 +1,8 @@
 import AsyncLock from "async-lock";
-import EventEmitter from "events";
 
 import ProviderWrappingOptions from "./provider-wrapping-options";
 import Provider from "./provider";
-import { DefaultState } from "../context";
+import { ContainerEventEmitter, DefaultState } from "../context";
 import { Middleware } from "../middleware";
 import bindInContext from "./bind-in-context";
 
@@ -19,19 +18,19 @@ function inContainerScope<T, State = DefaultState>(
   provider: Provider<T>,
   options?: ProviderWrappingOptions
 ): Middleware<T | T[], State> {
-  const cache = new Map<EventEmitter, T>();
-  const handleOnClose = (container: EventEmitter) => {
-    cache.delete(container);
+  const cache = new Map<string, T>();
+  const handleOnClose = (container: ContainerEventEmitter) => {
+    cache.delete(container.id);
   };
   const lock = new AsyncLock();
 
   return async (context, next) => {
     await lock.acquire(context.container.id, async () => {
-      let value = cache.get(context.container);
+      let value = cache.get(context.container.id);
       if (value == null) {
         value = await provider(context);
 
-        cache.set(context.container, value);
+        cache.set(context.container.id, value);
 
         context.container.addListener("close", handleOnClose);
         context.container.emit("create", value);
