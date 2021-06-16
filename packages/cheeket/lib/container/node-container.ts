1b2c27e87c3fb34f86929c33395b4a069a16665c
siyual-park <kdpark0723@gmail.com>
2021-06-17 00:59:36 +0900

    Add emitAsync

--- packages/cheeket/lib/container/node-container.ts
+++ packages/cheeket/lib/container/node-container.ts
@@ -92,6 +92,28 @@ class NodeContainer<State = DefaultState>
   close(): void {
     this.emit("close", this);
   }
+
+  emitAsync(event: string | symbol, ...args: unknown[]): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      try {
+        const count = this.listenerCount(event);
+        if (count === 0) {
+          resolve(false);
+          return;
+        }
+
+        let current = 0;
+        this.emit(event, ...args, () => {
+          current += 1;
+          if (current >= count) {
+            resolve(true);
+          }
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
 }
 
 export default NodeContainer;
--- packages/cheeket/lib/context/container-event-emitter.ts
+++ packages/cheeket/lib/context/container-event-emitter.ts
@@ -2,6 +2,8 @@ import EventEmitter from "events";
 
 interface ContainerEventEmitter extends EventEmitter {
   readonly id: string;
+
+  emitAsync(event: string | symbol, ...args: never[]): Promise<boolean>;
 }
 
 export default ContainerEventEmitter;
