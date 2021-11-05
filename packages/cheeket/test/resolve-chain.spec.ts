b497bc2424dec8c802c6fe83086e2f8209df115b
ara.bak <siyual.bak@gmail.com>
2021-11-05 11:37:46 +0900

    add test

--- packages/cheeket/test/resolve-chain.spec.ts
+++ packages/cheeket/test/resolve-chain.spec.ts
@@ -1,20 +1,23 @@
-/* eslint-disable no-plusplus,@typescript-eslint/no-loop-func */
+/* eslint-disable no-plusplus,@typescript-eslint/no-loop-func,max-classes-per-file */
 
 import ProviderStorage from "../lib/provider-storage";
 import ResolveChain from "../lib/resolve-chain";
 
-class Dummy {}
+class Dummy1 {}
+class Dummy2 {}
 
 describe("ResolveChain", () => {
   test("resolve", async () => {
     const storage = new ProviderStorage();
     const chain = new ResolveChain(storage);
 
-    storage.set(Dummy, (context) => {
-      context.response = new Dummy();
+    storage.set(Dummy1, (context) => {
+      context.response = new Dummy1();
+
+      expect(context.request).toEqual(Dummy1);
     });
 
-    expect(await chain.resolve(Dummy)).toBeTruthy();
+    expect(await chain.resolve(Dummy1)).toBeTruthy();
   });
 
   test("resolve: chaining", async () => {
@@ -24,11 +27,11 @@ describe("ResolveChain", () => {
     const childStorage = new ProviderStorage();
     const childChain = new ResolveChain(childStorage, parentChain);
 
-    parentStorage.set(Dummy, (context) => {
-      context.response = new Dummy();
+    parentStorage.set(Dummy1, (context) => {
+      context.response = new Dummy1();
     });
 
-    expect(await childChain.resolve(Dummy)).toBeTruthy();
+    expect(await childChain.resolve(Dummy1)).toBeTruthy();
   });
 
   test("resolve: multi provider", async () => {
@@ -38,35 +41,56 @@ describe("ResolveChain", () => {
     const childStorage = new ProviderStorage();
     const childChain = new ResolveChain(childStorage, parentChain);
 
-    childStorage.set(Dummy, async (context, next) => {
+    childStorage.set(Dummy1, async (context, next) => {
       await next();
     });
-    childStorage.set(Dummy, async (context, next) => {
-      context.response = new Dummy();
+    childStorage.set(Dummy1, async (context, next) => {
+      context.response = new Dummy1();
       await next();
     });
-    parentStorage.set(Dummy, () => {
+    parentStorage.set(Dummy1, () => {
       throw new Error();
     });
 
-    expect(await childChain.resolve(Dummy)).toBeTruthy();
+    expect(await childChain.resolve(Dummy1)).toBeTruthy();
+  });
+
+  test("resolve: nested", async () => {
+    const storage = new ProviderStorage();
+    const chain = new ResolveChain(storage);
+
+    storage.set(Dummy1, async (context) => {
+      await context.resolve(Dummy2);
+      context.response = new Dummy1();
+
+      expect(context.request).toEqual(Dummy1);
+    });
+    storage.set(Dummy2, async (context) => {
+      context.response = new Dummy2();
+
+      expect(context.request).toEqual(Dummy2);
+      expect(context.parent).toBeTruthy();
+      expect(context.parent?.children[0]).toEqual(context);
+    });
+
+    expect(await chain.resolve(Dummy1)).toBeTruthy();
   });
 
   test("resolveOrDefault", async () => {
     const storage = new ProviderStorage();
     const chain = new ResolveChain(storage);
 
-    storage.set(Dummy, (context) => {
-      context.response = new Dummy();
+    storage.set(Dummy1, (context) => {
+      context.response = new Dummy1();
     });
 
-    expect(await chain.resolveOrDefault(Dummy, null)).toBeTruthy();
+    expect(await chain.resolveOrDefault(Dummy1, null)).toBeTruthy();
   });
 
   test("resolveOrDefault: not resolved", async () => {
     const storage = new ProviderStorage();
     const chain = new ResolveChain(storage);
 
-    expect(await chain.resolveOrDefault(Dummy, null)).toBe(null);
+    expect(await chain.resolveOrDefault(Dummy1, null)).toBe(null);
   });
 });
