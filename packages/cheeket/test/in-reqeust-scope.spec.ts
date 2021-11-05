eacdb966cbaef1283fe7d14439004ded41a8ed89
ara.bak <siyual.bak@gmail.com>
2021-11-05 12:11:49 +0900

    add test

---
+++ packages/cheeket/test/in-reqeust-scope.spec.ts
@@ -0,0 +1,110 @@
+/* eslint-disable no-plusplus,@typescript-eslint/no-loop-func,max-classes-per-file */
+
+import {
+  bindArray,
+  bindObject,
+  Container,
+  inRequestScope,
+  Token,
+} from "../lib";
+
+class Dummy {}
+const DummyArray = Symbol("Dummy[]") as Token<Dummy[]>;
+
+describe("inRequestScope", () => {
+  test("bindObject", async () => {
+    const container1 = new Container();
+    const container2 = new Container();
+
+    const provider = inRequestScope(() => new Dummy(), bindObject());
+
+    container1.register(Dummy, provider);
+    container2.register(Dummy, provider);
+
+    const dummy1 = await container1.resolve(Dummy);
+    const dummy2 = await container1.resolve(Dummy);
+
+    expect(dummy1).not.toBe(dummy2);
+
+    const dummy3 = await container2.resolve(Dummy);
+    const dummy4 = await container2.resolve(Dummy);
+
+    expect(dummy3).not.toBe(dummy4);
+    expect(dummy1).not.toBe(dummy3);
+  });
+
+  test("bindObject: child", async () => {
+    const parent = new Container();
+    const child = parent.createChild();
+    const provider = inRequestScope(() => new Dummy(), bindObject());
+
+    parent.register(Dummy, provider);
+
+    const dummy1 = await child.resolve(Dummy);
+    const dummy2 = await child.resolve(Dummy);
+
+    expect(dummy1).not.toBe(dummy2);
+
+    const dummy3 = await parent.resolve(Dummy);
+    const dummy4 = await parent.resolve(Dummy);
+
+    expect(dummy3).not.toBe(dummy4);
+    expect(dummy1).not.toBe(dummy3);
+
+    parent.clear();
+  });
+
+  test("bindArray", async () => {
+    const container1 = new Container();
+    const container2 = new Container();
+
+    const provider1 = inRequestScope(() => new Dummy(), bindArray());
+    const provider2 = inRequestScope(() => new Dummy(), bindArray());
+
+    container1.register(DummyArray, provider1);
+    container1.register(DummyArray, provider2);
+
+    container2.register(DummyArray, provider1);
+    container2.register(DummyArray, provider2);
+
+    const dummy1 = await container1.resolve(DummyArray);
+    const dummy2 = await container1.resolve(DummyArray);
+
+    expect(dummy1.length).toBe(2);
+    expect(dummy2.length).toBe(2);
+
+    expect(dummy1[0]).not.toBe(dummy2[0]);
+    expect(dummy1[1]).not.toBe(dummy2[1]);
+
+    const dummy3 = await container2.resolve(DummyArray);
+    const dummy4 = await container2.resolve(DummyArray);
+
+    expect(dummy3.length).toBe(2);
+    expect(dummy4.length).toBe(2);
+
+    expect(dummy3[0]).not.toBe(dummy4[0]);
+    expect(dummy3[1]).not.toBe(dummy4[1]);
+
+    expect(dummy1[0]).not.toBe(dummy3[0]);
+    expect(dummy1[1]).not.toBe(dummy3[1]);
+  });
+
+  test("bindArray: child", async () => {
+    const parent = new Container();
+    const child = parent.createChild();
+
+    const provider1 = inRequestScope(() => new Dummy(), bindObject());
+    const provider2 = inRequestScope(() => new Dummy(), bindArray());
+
+    parent.register(DummyArray, provider1);
+    child.register(DummyArray, provider2);
+
+    const dummy1 = await child.resolve(DummyArray);
+    const dummy2 = await child.resolve(DummyArray);
+
+    expect(dummy1.length).toBe(1);
+    expect(dummy2.length).toBe(1);
+
+    expect(dummy1[0]).not.toBe(dummy2[0]);
+  });
+});
