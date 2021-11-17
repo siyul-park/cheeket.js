6ab4504d80d286a687eff75352cd95d191073a0d
Ara Park <siyual.bak@gmail.com>
2021-11-17 14:43:07 +0900

    hotfix: fix export

--- packages/cheeket-koa-module/lib/index.ts
+++ packages/cheeket-koa-module/lib/index.ts
@@ -3,3 +3,5 @@ export { default as ModuleManager } from "./module-manager";
 
 export { default as modules } from "./modules";
 export { default as local } from "./local";
+
+export { default as InternalTokens } from "./internal-tokens";
