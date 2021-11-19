a1f628bc79f4244481e9dea67596ddb1126de1dc
Ara Park <siyual.bak@gmail.com>
2021-11-19 10:33:27 +0900

    feat: change module style

--- packages/cheeket-koa-module/lib/simple-module.ts
+++ packages/cheeket-koa-module/lib/simple-module.ts
@@ -2,25 +2,36 @@ import { DefaultContext, DefaultState, Middleware } from "koa";
 import { ContainerContext } from "cheeket-koa";
 import { Container } from "cheeket";
 import Module from "./module";
+import compose from "koa-compose";
 
 abstract class SimpleModule<ContextT = DefaultContext> implements Module<ContextT> {
   private readonly globalContainers = new Set<Container>();
+
+  private readonly middlewares: Middleware<DefaultState, ContextT>[] = [];
+
+  use(...middleware: Middleware<DefaultState, ContextT>[]): this {
+    this.middlewares.push(...middleware);
+    return this;
+  }
 
   protected abstract configureGlobal(container: Container): void;
 
   protected abstract configureLocal(container: Container): void;
 
   modules(): Middleware<DefaultState, ContextT & ContainerContext> {
-    return async (context, next) => {
-      if (!this.globalContainers.has(context.containers.global)) {
-        this.globalContainers.add(context.containers.global);
-        this.configureGlobal(context.containers.global);
-      }
+    return compose([
+      async (context, next) => {
+        if (!this.globalContainers.has(context.containers.global)) {
+          this.globalContainers.add(context.containers.global);
+          this.configureGlobal(context.containers.global);
+        }
 
-      this.configureLocal(context.containers.local);
+        this.configureLocal(context.containers.local);
 
-      await next();
-    };
+        await next();
+      },
+      ...this.middlewares,
+    ]);
   }
 }
 
