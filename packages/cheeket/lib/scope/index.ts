ddc75c63a5656077f0db683c06eecf1502cfa6d6
siyual-park <siyual.bak@gmail.com>
2022-04-11 09:00:36 +0900

    feat: remove global scope

--- packages/cheeket/lib/scope/index.ts
+++ packages/cheeket/lib/scope/index.ts
@@ -1,3 +1,2 @@
 export { default as containerScope, ContainerScope } from './container-scope';
-export { default as globalScope, GlobalScope } from './global-scope';
 export { default as requestScope, RequestScope } from './request-scope';
